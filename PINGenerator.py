95e7bfc9e3a156c1ebad2dbe6312206819adc01d
Muyu Chen <42565411+Muyu-Chen@users.noreply.github.com>
2025-01-09 20:32:55 -0500

    Add functionality to generate multiple card keys and output them to the console

--- PINGenerator.py
+++ PINGenerator.py
@@ -1,4 +1,5 @@
 from checkCardPIN import generateCardPINFunc, parse_card_key
+import json
 
 # Generate a card PIN
 # option = 0: only this UID is valid
@@ -9,8 +10,45 @@ from checkCardPIN import generateCardPINFunc, parse_card_key
 priceLevel = 100
 uid = "12345678"
 option = 1
-cardPIN = generateCardPINFunc(priceLevel, uid, option)
-print("copy this to the cardPIN.json : " + str(cardPIN[1]))
-# decode
-parsed_uid, parsed_price = parse_card_key(cardPIN[1])
-print(parsed_uid)
+
+
+if option == 0:
+    optionContent = "only this UID is valid"
+elif option == 1:
+    optionContent = "the PIN for all UIDs are valid"
+elif option == 2:
+    optionContent = "the PIN for all UIDs are valid, you can add other usage scenarios"
+
+
+def genMuti(a):
+    print("---------------------------")
+    print("the number of cardPIN: " + str(a))
+    cardPIN = []
+    for i in range(a):
+        # generate
+        cardPINtemp = generateCardPINFunc(priceLevel, uid, option)
+        cardPIN.append(cardPINtemp[1])
+        # print("copy this to the cardPIN.json: " + str(cardPINtemp[1]))
+
+        # check the generated cardPIN
+        parsed_uid, parsed_price = parse_card_key(cardPINtemp[1])
+        print("parsed uid:", parsed_uid, "price:", parsed_price)
+        print("-----------")
+        # opt0Str = "3408"
+        # opt1Str = "8196"
+        # opt2Str = "5072"
+    cardPIN = (
+        json.dumps(cardPIN)
+        .replace("[", "")
+        .replace("]", "")
+        .replace(",", ",\n")
+        .replace(" ", "")
+    )
+    print("---------------------------")
+    print("the price: " + str(priceLevel))
+    print("the cardPIN option：" + str(optionContent))
+    print("copy this to cardPIN.json: \n--------\n" + cardPIN)
+
+
+# change this number to generate more cardPIN
+genMuti(10)
